fc59879663d0a136a419c7de0165f2af4f99c5bd
daorejuela1 <vids@hotmail.es>
2020-07-21 22:39:31 +0000

    uploaded task 6 for pre order print

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_preorder - prints the binary tree in pre order
+ *@tree: value of binary tree
+ *@func: function to print the node
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
