98831639d78d017745dc24dec9e5f6f5a7d9b479
Javier <ingenierodesoftware73@gmail.com>
2020-07-23 02:34:10 +0000

    upload task 15 binary tree is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_is_full - is full or no full
+ *@tree: value of binary tree
+ *Return: value of full
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int c1 = 0;
+
+	if (tree == NULL)
+		return (0);
+	return (count(&c1, tree));
+}
+/**
+ *count - counter of nodes
+ *@tree: value of binary tree
+ *@c1: counter of nodes
+ *Return: value of count of nodes
+ */
+int count(int *c1, const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL && tree->right != NULL)
+		*c1 = 1;
+	else if (tree->left == NULL && tree->right != NULL)
+		*c1 = 0;
+	else if (tree->right == NULL && tree->left != NULL)
+		*c1 = 0;
+	count(c1, tree->left);
+	count(c1, tree->right);
+	return (*c1);
+}
